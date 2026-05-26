4745e81c3ec51b25a5ebc5a08a74c9eba3df4c36
thiagoguimaraessf-afk <thiagoguimaraessf@gmail.com>
2026-05-25 23:34:06 -0300

    Notas na Base

--- Script.py
+++ Script.py
@@ -1,6 +1,12 @@
-# Tabela
+# Estrutura de dados
 
+# Memoria Ran - Ela era definir um ID para cada posição de memória, e o valor armazenado nessa posição. Por exemplo, a posição de memória 0 poderia armazenar o valor 5, a posição de memória 1 poderia armazenar o valor 10, e assim por diante.
 
+# Registradores - Uma menoria rapida, onde ficaram guardada as corisas que estão sendo processadas no momento. Por exemplo, um registrador poderia armazenar o resultado de uma operação de adição, ou o endereço de memória onde um valor está armazenado. (So precisam salvar numeros inteiros) (so existem 4 registradores, R0, R1, R2 e R3)
+
+# Programa Counter (PC) - Um contador que indica a posição atual do programa que está sendo executado. Ele é incrementado a cada instrução executada, e pode ser alterado por comandos de salto (como o comando JMP) para pular para uma posição específica do programa.
+
+# ULA - Unidade Lógica e Aritmética, responsável por realizar operações matemáticas e lógicas. Ela pode realizar operações como adição, subtração, multiplicação, divisão, AND, OR, NOT, entre outras. A ULA recebe os operandos (valores a serem processados) dos registradores e retorna o resultado da operação para um registrador ou para a memória.
 
 # Comandos
 
@@ -17,8 +23,7 @@ def SUB():
 # Comando - JMP (pular)
 def JMP():
     print("Comando JMP executado")
-
-
+    
 # Comando - LOAD (carregar)
 def LOAD():
     print("Comando LOAD executado")
@@ -43,16 +48,20 @@ def Help():
     print("help - Mostrar ajuda")
 
 # Inicicialização
+
 print("Bem-vindo ao simulador de computador!")
+
 print("Digite 'Help' para ver os comandos disponíveis.")
-while True:
-    comando = input("Digite um comando: ")
-    
-    if comando == "add":
+
+while True: # Loop infinito para continuar pedindo comandos até que o usuário decida parar
+
+    comando = input("Digite um comando: ") # Ta peidendop um comando do usuário
+
+    if comando == "add": # se o comando for "add", chama a função ADD()
         ADD()
-    elif comando == "sub":
+    elif comando == "sub": # se o comando for "sub", chama a função SUB()
         SUB()
-    elif comando == "jmp":
+    elif comando == "jmp": # Assim por diante
         JMP()
     elif comando == "load":
         LOAD()
@@ -64,5 +73,5 @@ while True:
     elif comando == "clear":
         Clear()
     else:
-        print("Comando desconhecido. Digite 'Help' para ver os comandos disponíveis.")
+        print("Comando desconhecido. Digite 'Help' para ver os comandos disponíveis.") # Se o comando não for reconhecido, mostra uma mensagem de erro e pede para digitar 'Help' para ver os comandos disponíveis.
 
