986016c4ab75bf170a13959fd416236f4f43b539
thiagoguimaraessf-afk <thiagoguimaraessf@gmail.com>
2026-05-30 14:08:58 -0300

    Algumas alterações

--- Script.py
+++ Script.py
@@ -2,9 +2,19 @@
 
 # Memoria Ran - Ela era definir um ID para cada posição de memória, e o valor armazenado nessa posição. Por exemplo, a posição de memória 0 poderia armazenar o valor 5, a posição de memória 1 poderia armazenar o valor 10, e assim por diante.
 
+menoria_ran = [0] * 100 # Inicializa a memória RAM com 256 posições, todas com o valor 0
+
+
 # Registradores - Uma menoria rapida, onde ficaram guardada as corisas que estão sendo processadas no momento. Por exemplo, um registrador poderia armazenar o resultado de uma operação de adição, ou o endereço de memória onde um valor está armazenado. (So precisam salvar numeros inteiros) (so existem 4 registradores, R0, R1, R2 e R3)
 
+R0 = 0 # Registrador 0
+R1 = 0 # Registrador 1
+R2 = 0 # Registrador 2
+R3 = 0 # Registrador 3
+
 # Programa Counter (PC) - Um contador que indica a posição atual do programa que está sendo executado. Ele é incrementado a cada instrução executada, e pode ser alterado por comandos de salto (como o comando JMP) para pular para uma posição específica do programa.
+
+PC = 0 # Inicializa o Programa Counter (PC) com o valor 0
 
 # ULA - Unidade Lógica e Aritmética, responsável por realizar operações matemáticas e lógicas. Ela pode realizar operações como adição, subtração, multiplicação, divisão, AND, OR, NOT, entre outras. A ULA recebe os operandos (valores a serem processados) dos registradores e retorna o resultado da operação para um registrador ou para a memória.
 
@@ -23,7 +33,16 @@ def SUB():
 # Comando - JMP (pular)
 def JMP():
     print("Comando JMP executado")
+
+    R0 = int(input("Digite o endereço para pular: "))
     
+    if R0 < 0 or R0 >= len(menoria_ran):
+        print("Endereço inválido. O endereço deve estar entre 0 e", len(menoria_ran) - 1)
+    else:
+        global PC
+        PC = R0
+        print("Programa Counter (PC) atualizado para:", PC)
+            
 # Comando - LOAD (carregar)
 def LOAD():
     print("Comando LOAD executado")
